a6c14c29d97869984ca11e14b4c477b3db36e7d5
Ryan <Ryan@CROCODILE>
2018-08-07 12:32:20 -0400

    add-migrations update-database

--- again/Migrations/20180807163116_newModels.Designer.cs
+++ again/Migrations/20180807163116_newModels.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using again.Models;
 
 namespace again.Migrations
 {
     [DbContext(typeof(againContext))]
-    partial class againContextModelSnapshot : ModelSnapshot
+    [Migration("20180807163116_newModels")]
+    partial class newModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ again/Migrations/20180807163116_newModels.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace again.Migrations
+{
+    public partial class newModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlayerID",
+                table: "Game",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    PlayerID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.PlayerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Library",
+                columns: table => new
+                {
+                    LibraryID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PlayerID = table.Column<int>(nullable: false),
+                    GameID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Library", x => x.LibraryID);
+                    table.ForeignKey(
+                        name: "FK_Library_Game_GameID",
+                        column: x => x.GameID,
+                        principalTable: "Game",
+                        principalColumn: "GameID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Library_Player_PlayerID",
+                        column: x => x.PlayerID,
+                        principalTable: "Player",
+                        principalColumn: "PlayerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Game_PlayerID",
+                table: "Game",
+                column: "PlayerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Library_GameID",
+                table: "Library",
+                column: "GameID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Library_PlayerID",
+                table: "Library",
+                column: "PlayerID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Game_Player_PlayerID",
+                table: "Game",
+                column: "PlayerID",
+                principalTable: "Player",
+                principalColumn: "PlayerID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Game_Player_PlayerID",
+                table: "Game");
+
+            migrationBuilder.DropTable(
+                name: "Library");
+
+            migrationBuilder.DropTable(
+                name: "Player");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Game_PlayerID",
+                table: "Game");
+
+            migrationBuilder.DropColumn(
+                name: "PlayerID",
+                table: "Game");
+        }
+    }
+}
