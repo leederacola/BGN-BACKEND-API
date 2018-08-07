af684bd1c89e3db45efbccd6da8568e597cdc546
Ryan <Ryan@CROCODILE>
2018-08-07 10:29:41 -0400

    Add project files.

---
+++ again/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace again.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
