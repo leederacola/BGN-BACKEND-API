ba1a22697eaf7955884ddcad7947b0dd2640ff3d
Ryan <Ryan@CROCODILE>
2018-08-13 14:43:58 -0400

    player api controller

---
+++ again/Controllers/PlayerApiController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using again.Models;
+using again.Interface;
+using Microsoft.AspNetCore.Cors;
+using System.Collections.Generic;
+
+namespace again.Controllers
+{
+    [Route("api/players")]
+    [EnableCors("LocalTest")]
+    [ApiController]
+    public class PlayerApiController : ControllerBase
+    {
+        private readonly IPlayerRepository _playerRepository;
+
+        public PlayerApiController(IPlayerRepository playerRepository)
+        {
+            _playerRepository = playerRepository;
+        }
+
+        //GET: api/players
+        [Route("")]
+        [HttpGet]
+        public Task<IEnumerable<Player>> GetAllPlayers()
+        {
+            var players = _playerRepository.GetAllPlayers();
+            return players;
+        }
+    }
+}
