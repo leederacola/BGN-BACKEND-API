c89a44b2dbcb997f9b3b2e5fe042e6281b70280d
Ryan <Ryan@CROCODILE>
2018-09-07 14:47:11 -0400

    added interface and reposityot

---
+++ again/Concrete/EventRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using again.Interface;
+using again.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace again.Concrete
+{
+    public class EventRepository : IEventRepository
+    {
+        private readonly againContext _dbContext;
+
+        public EventRepository(againContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //gets azll events ordered by date
+        public async Task<IEnumerable<Event>> GetAllEvents()
+        {
+            return await _dbContext.Event.OrderBy(e => e.DateTime).ToListAsync();
+        }
+
+        //get events by Active status 
+        public async Task<IEnumerable<Event>> GetEventsActive(bool isActive)
+        {
+            return await _dbContext.Event.Where(e => e.Active == isActive).ToListAsync();
+        }
+    }
+}
---
+++ again/Interface/IEventrepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using again.Models;
+
+namespace again.Interface
+{
+    public interface IEventRepository
+    {
+        Task<IEnumerable<Event>> GetAllEvents();
+        Task<IEnumerable<Event>> GetEventsActive(bool isActive);
+    }
+}
